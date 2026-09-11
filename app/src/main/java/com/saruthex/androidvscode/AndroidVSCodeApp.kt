8814410ac2885b2a05b663d19d65bd0ade551829
SARTHAK GUND <83057972+saruthex@users.noreply.github.com>
2026-09-11 13:49:51 +0530

    Add required Compose layout weight import for Phase 2

--- app/src/main/java/com/saruthex/androidvscode/AndroidVSCodeApp.kt
+++ app/src/main/java/com/saruthex/androidvscode/AndroidVSCodeApp.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.weight
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
